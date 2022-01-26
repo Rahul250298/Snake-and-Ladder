9d34f64b0ffe6c5f31f114ee590977744704bb5d
Rahul250298 <91786240+Rahul250298@users.noreply.github.com>
2022-01-26 10:53:14 +0530

    adding Check for option

---
+++ Snake and Ladder/UC3.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SnakeAndLadder
+    {
+         public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        public int StartPoint = 0;
+        public int DiceRoll;
+        int Option = 0;
+        public void StartPlaying()
+        {
+            Random Random = new Random();
+            DiceRoll = Random.Next(1, 7);
+            Console.WriteLine("player Roll  Dice number is:" + " " + DiceRoll);
+            Option = Random.Next(0, 3);
+            Console.WriteLine("Dice value:" + Option);
+            switch (Option)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case SNAKE:
+                    StartPoint += DiceRoll;
+                    Console.WriteLine("Got Ladder:" + StartPoint);
+                    break;
+                case LADDER:
+                    StartPoint -= DiceRoll;
+                    Console.WriteLine("Snake Attack:" + StartPoint);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+        }
+    }
+}
