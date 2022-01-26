e52a465bd7b742e67e12a4bbfb9afac49dea35c2
Rahul250298 <91786240+Rahul250298@users.noreply.github.com>
2022-01-26 10:22:22 +0530

    Add files via upload
    adding files

---
+++ Snake and Ladder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder problem");
+
+            SnakeAndLadder Player = new SnakeAndLadder();
+            Player.StartPlaying();
+            Console.ReadKey();
+        }
+    }
+}
