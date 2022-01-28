6aaf0d2d5557d23ea1d752aa66e7098018460be2
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-01-28 20:34:33 +0530

    Update UC3.cs

--- Snake and Ladder/UC3.cs
+++ Snake and Ladder/UC3.cs
@@ -8,7 +8,7 @@ namespace SnakeLadder
 {
     public class SnakeAndLadder
     {
-         public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
         public int StartPoint = 0;
         public int DiceRoll;
         int Option = 0;
@@ -24,11 +24,11 @@ namespace SnakeLadder
                 case NO_PLAY:
                     Console.WriteLine("No Play");
                     break;
-                case SNAKE:
+                case LADDER:
                     StartPoint += DiceRoll;
                     Console.WriteLine("Got Ladder:" + StartPoint);
                     break;
-                case LADDER:
+                case SNAKE:
                     StartPoint -= DiceRoll;
                     Console.WriteLine("Snake Attack:" + StartPoint);
                     break;
